f53b042326fe8e147799a871c995b0ce37ec92c9
Rahul626 <rj7407@gmail.com>
2019-08-23 20:09:08 +0530

    setting up

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('first middleware');
+    next();
+});
+
+
+app.use((req, res, next) => {
+    res.send('hello from express');
+    next();
+});
+
+
+module.exports = app;
