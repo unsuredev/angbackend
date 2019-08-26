5671e18326441bfdc7d3a7f3314c58710d4f2902
Rahul626 <rj7407@gmail.com>
2019-08-26 15:17:05 +0530

    updated second day

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,47 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    console.log('first middleware');
-    next();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+     res.setHeader("Access-Control-Allow-Header",
+        "Origin, X-Requested-With,Content-type, Accept "
+    );
+res.setHeader("Access-Control-Allow-Methods", 
+"GET, POST,PATCH,DELETE. OPTIONS"
+);
+next();
 });
 
+app.post('/api/posts', (req, res, next) =>
+{
+    console.log)()
+})
 
-app.use((req, res, next) => {
-    res.send('hello from express');
-    next();
+
+app.use('/api/posts', (req, res, next) => {
+
+    const posts = [
+
+        {
+            id: '8552585',
+            title: 'this is first serverside title',
+            content: 'this is coming from server'
+
+        },
+        {
+            id: '855258fdfg5',
+            title: 'this is second serverside title',
+            content: 'this is coming from server!!!!!!!! second'
+
+        }
+    ];
+    return res.status(200).json(
+        {
+            message: 'post messsge successfully',
+            posts: posts
+        }
+    );
+
+
 });
 
 
