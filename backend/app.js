99b7dc90ea7001465646e4ab22c0651c3b5c8699
Rahul626 <rj7407@gmail.com>
2019-08-27 20:06:06 +0530

    8th day

--- backend/app.js
+++ backend/app.js
@@ -1,64 +1,65 @@
-const express = require('express');
+const express = require("express");
+const bodyparser = require("body-parser");
+
+const mongoose = require("mongoose");
+const Post = require("../models/post");
 const app = express();
-const bodyparser = require('body-parser');
+
+mongoose
+  .connect(
+    "mongodb+srv://angblog:vNSBXJJWihXx9Ai6@cluster0-wepoo.mongodb.net/angblog?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected to database ");
+  })
+
+  .catch(() => {
+    console.log();
+  });
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
-
-const Post = require('../models/post');
-
-
-app.use((req, res, next) => {
+pp.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With,Content-type, Accept "
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
-    res.setHeader("Access-Control-Allow-Methods",
-        "GET, POST,PATCH,DELETE. OPTIONS"
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, DELETE, OPTIONS"
     );
-});
-
-app.post('/api/posts', (req, res, next) => {
+    next();
+  });
+  
+  app.post("/api/posts", (req, res, next) => {
     const post = new Post({
-        title: req.body.title,
-        content: req.body.content
-
+      title: req.body.title,
+      content: req.body.content
     });
-
-    console.log(post);
-    res.status(201).json({
-        message: "post send successfully"
+    post.save().then(createdPost => {
+      res.status(201).json({
+        message: "Post added successfully",
+        postId: createdPost._id
+      });
     });
-
-});
-
-
-app.get('/api/posts', (req, res, next) => {
-
-    const posts = [
-
-        {
-            id: '8552585',
-            title: 'this is first serverside title',
-            content: 'this is coming from server'
-
-        },
-        {
-            id: '855258fdfg5',
-            title: 'this is second serverside title',
-            content: 'this is coming from server!!!!!!!! second'
-
-        }
-    ];
-    return res.status(200).json(
-        {
-            message: 'post messsge successfully',
-            posts: posts
-        }
-    );
-
-
-});
-
-
-module.exports = app;
+  });
+  
+  app.get("/api/posts", (req, res, next) => {
+    Post.find().then(documents => {
+      res.status(200).json({
+        message: "Posts fetched successfully!",
+        posts: documents
+      });
+    });
+  });
+  
+  app.delete("/api/posts/:id", (req, res, next) => {
+    Post.deleteOne({ _id: req.params.id }).then(result => {
+      console.log(result);
+      res.status(200).json({ message: "Post deleted!" });
+    });
+  });
+  
+  module.exports = app;
+  
