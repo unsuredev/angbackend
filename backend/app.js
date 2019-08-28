74627a468cc5be6ee64f9d19d9e9c9a937189c7d
Rahul626 <rj7407@gmail.com>
2019-08-28 20:20:42 +0530

    router added

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
-const express = require("express");
+const express = require("express"); //to import express
 const bodyparser = require("body-parser");
+const mongoose = require("mongoose"); //to access and handle mongoDb
 
-const mongoose = require("mongoose");
-const Post = require("../models/post");
 const app = express();
 
+const postRoutes = require("../routes/posts");
+
+/*********************  my mongodb atlas connection link***************** */
 mongoose
   .connect(
     "mongodb+srv://angblog:vNSBXJJWihXx9Ai6@cluster0-wepoo.mongodb.net/angblog?retryWrites=true&w=majority"
@@ -19,47 +21,18 @@ mongoose
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
-pp.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, DELETE, OPTIONS"
-    );
-    next();
-  });
-  
-  app.post("/api/posts", (req, res, next) => {
-    const post = new Post({
-      title: req.body.title,
-      content: req.body.content
-    });
-    post.save().then(createdPost => {
-      res.status(201).json({
-        message: "Post added successfully",
-        postId: createdPost._id
-      });
-    });
-  });
-  
-  app.get("/api/posts", (req, res, next) => {
-    Post.find().then(documents => {
-      res.status(200).json({
-        message: "Posts fetched successfully!",
-        posts: documents
-      });
-    });
-  });
-  
-  app.delete("/api/posts/:id", (req, res, next) => {
-    Post.deleteOne({ _id: req.params.id }).then(result => {
-      console.log(result);
-      res.status(200).json({ message: "Post deleted!" });
-    });
-  });
-  
-  module.exports = app;
-  
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST,PUT, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+module.exports = app;
+app.use("api/post", postRoutes);
